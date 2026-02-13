a3b737037c6e3a746bc9ebc42e271d79faf86ea0
patrickfitzgerald <fitzgepc@mail.uc.edu>
2026-02-13 17:26:14 -0500

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    Scanner input = new Scanner(System.in);
+    String menuChoice = "";
+    String menu = "What is your party affiliation D - Democrat\tR - Republican\tI - Independent\t - Other?";
+
+    System.out.print(menu);
+    menuChoice = input.nextLine();
+
+    if(menuChoice.equalsIgnoreCase("D"))
+    {
+        System.out.println("You get a Donkey!");
+    }
+    else if (menuChoice.equalsIgnoreCase("R"))
+    {
+        System.out.println("You get a Elephant!");
+    }
+    else if (menuChoice.equalsIgnoreCase("I"))
+    {
+        System.out.println("You get a Person!");
+    }
+    else
+    {
+        System.out.println("You get a Other!");
+    }
+
     }
 }
